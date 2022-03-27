cfb6aba6f04db69cb0db2887aaecba710cb651b3
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-03-27 19:41:36 +0100

    thoghts schema and send tojson

--- models/Thought.js
+++ models/Thought.js
@@ -29,3 +29,31 @@ const ReactionSchema = new Schema(
 		}
 	}
 );
+const ThoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: true,
+			minlength: 1,
+			maxlength: 280
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+		},
+		username: {
+			type: String,
+			required: true,
+			ref: 'User'
+		},
+		reactions: [ReactionSchema],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		id: false
+	}
+)
