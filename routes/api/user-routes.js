a08d67ad1bd4d43df0f66e34fd5c7da3e685905e
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-03-27 19:27:13 +0100

    user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,3 +8,20 @@ const {
 	deleteUser,
 	removeFriend
 } = require('../../controllers/user-controller');
+router
+	.route('/')
+	.get(getAllUsers)
+	.post(createUser);
+
+router
+	.route('/:id')
+	.get(getUserById)
+	.put(updateUser)
+	.delete(deleteUser)
+
+router
+	.route('/:userId/friends/:friendId')
+	.post(addFriend)
+	.delete(removeFriend)
+
+module.exports = router;
